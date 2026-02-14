4cee1a420df2a11f1731f382fb6ebb8f92215888
GlennClaesPXL <Glenn.Claes@student.pxl.be>
2026-02-14 18:07:59 +0100

    Update

--- frontend/vitest.config.js
+++ frontend/vitest.config.js
@@ -8,7 +8,12 @@ export default mergeConfig(
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
-      root: fileURLToPath(new URL('./', import.meta.url)),
-    },
-  }),
+      coverage: {
+        provider: 'v8', // of 'istanbul' als je dat wilt
+        reporter: ['text', 'lcov'],
+        include: ['src/**/*.{js,vue}'], // zorgt dat coverage je App.vue en andere src-bestanden pakt
+        all: true
+      }
+    }
+  })
 )

13db892e676578b7ea10b02373f52bc5a837cd0a
GlennClaesPXL <Glenn.Claes@student.pxl.be>
2026-02-14 14:56:26 +0100

    Playwright

--- frontend/playwright.config.js
+++ frontend/playwright.config.js
@@ -26,9 +26,10 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 4 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  headless: true,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
@@ -98,13 +99,9 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    /**
-     * Use the dev server by default for faster feedback loop.
-     * Use the preview server on CI for more realistic testing.
-     * Playwright will re-use the local server if there is already a dev-server running.
-     */
-    command: process.env.CI ? 'npm run preview' : 'npm run dev',
-    port: process.env.CI ? 4173 : 5173,
-    reuseExistingServer: !process.env.CI,
-  },
+  command: process.env.CI ? 'npm run preview' : 'npm run dev',
+  port: 5173,
+  reuseExistingServer: !process.env.CI,
+  timeout: 120_000, // 2 minuten voor CI
+},
 })

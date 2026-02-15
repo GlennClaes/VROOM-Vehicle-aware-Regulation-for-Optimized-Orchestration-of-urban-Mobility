d8e5d2a17b700f4e52942747822214e1c5ee81ea
GlennClaesPXL <Glenn.Claes@student.pxl.be>
2026-02-15 20:44:23 +0100

    Bugfix

--- frontend/src/__tests__/router.spec.js
+++ frontend/src/__tests__/router.spec.js
@@ -3,7 +3,7 @@ import router from '../router/index'
 
 describe('Router', () => {
   it('should have Home and About routes', () => {
-    const routeNames = router.getRoutes().map(r => r.name)
+    const routeNames = router.getRoutes().map((r) => r.name)
     expect(routeNames).toContain('Home')
     expect(routeNames).toContain('About')
   })

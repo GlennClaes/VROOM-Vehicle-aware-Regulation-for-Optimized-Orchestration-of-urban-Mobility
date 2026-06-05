6f62cd77b7a86d8b3a952d6057642306ec4fad31
GlennClaesPXL <Glenn.Claes@student.pxl.be>
2026-06-05 09:36:15 +0200

    Added docs

---
+++ docs-site/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  base: '/VROOM-Vehicle-aware-Regulation-for-Optimized-Orchestration-of-urban-Mobility/'
+})
